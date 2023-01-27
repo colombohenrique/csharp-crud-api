5d88f367a240d4170f9421c6baa4a3b0baaa7e15
José Henrique Colombo <jhbcolombo@hotmail.com>
2023-01-27 15:17:46 -0300

    Adicionado Contexto

---
+++ RestAPI/RestAPI.Infra.Data/Context/RestAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using RestAPI.Domain.Entities;
+using System.Data.Entity;
+
+namespace RestAPI.Infra.Data.Context
+{
+    public class RestAPIContext : DbContext 
+    {
+        public RestAPIContext()
+            :base("Conexao") { }
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+}
+}
